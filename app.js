024cd804a10201f7a99929255600296e1982ccfb
AriStWeb <sole21008@gmail.com>
2025-10-17 03:47:16 -0300

    import and syntax correction

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv')
 const app = express();
 const dbConnect = require('./config/db');
-//const studentsRoutes = require('./routes/estudiantes');
+const studentsRoutes = require('./routes/students');
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
 dbConnect();
-
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Api estudiantes');
 });
 
-//app.use('/api', studentsRoutes);
+app.use('/api', studentsRoutes );
 
 app.listen(PORT, () => {
   console.log(`El servidor corre en el puerto ${PORT}`);
--- models/student.js
+++ models/student.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const nameCourse = ["matematica","historia","ciencia", "arte"];
 
 const Student = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  apellido: { type: String, required: true},
-  email: { type: String, required: true },
-  curso: [{ type: String, enum: nameCourse ,required: true}]
+nombre: { type: String, required: true },
+apellido: { type: String, required: true},
+email: { type: String, required: true },
+curso: [{ type: String, enum: nameCourse ,required: true}]
 },
 {timestamps:{
-    createdAt: 'fechaCreacion',
-    updatedAt: 'fechaActualizacion'
-  }}
+createdAt: 'fechaCreacion',
+updatedAt: 'fechaActualizacion'
+}}
 );
 
 module.exports = mongoose.model('Estudiante', Student);
