fa49b1bf98bcb592ac108b99558a56146f69b95d
AriStWeb <sole21008@gmail.com>
2025-10-17 01:44:41 -0300

    created student data model

---
+++ models/student.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const nameCourse = ["matematica","historia","ciencia", "arte"];
+
+const Student = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: true},
+  email: { type: String, required: true },
+  curso: [{ type: String, enum: nameCourse ,required: true}]
+},
+{timestamps:{
+    createdAt: 'fechaCreacion',
+    updatedAt: 'fechaActualizacion'
+  }}
+);
+
+module.exports = mongoose.model('Estudiante', Student);
